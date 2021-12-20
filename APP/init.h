2eb3822f4141c57a638fdf3acf5743d005ca2157
Color_yr <402067010@qq.com>
2021-12-20 13:22:27 +0800

    up

--- APP/init.h
+++
@@ -1,8 +0,0 @@
-#ifndef __INIT_H__
-#define __INIT_H__
-#include "sys.h"
-
-void init(void);
-void PWM_Init(u16, u16);
-
-#endif
--- APP/touch_check.h
+++
@@ -1,9 +0,0 @@
-#ifndef TOUCH_CHECK_H
-#define TOUCH_CHECK_H
-
-#include "sys.h"
-
-u8 button_check(void);
-void button_check1(void);
-
-#endif
