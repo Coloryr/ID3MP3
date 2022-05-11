417b4543567f3e6d1d615e6958259e2b818c7b08
Color_yr <402067010@qq.com>
2022-05-11 19:05:53 +0800

    Update init.cpp

--- src/Core/Src/Init/init.cpp
+++ src/Core/Src/Init/init.cpp
@@ -441,6 +441,13 @@ void MX_GPIO_Init() {
     GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
     LL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
+    GPIO_InitStruct.Pin = LL_GPIO_PIN_4;
+    GPIO_InitStruct.Mode = LL_GPIO_MODE_OUTPUT;
+    GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_VERY_HIGH;
+    GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
+    GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
+    LL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
     GPIO_InitStruct.Pin = LL_GPIO_PIN_6;
     LL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
